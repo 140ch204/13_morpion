629357e78a05db608cdfdd830986f6a97d25b33d
140ch204 <140ch204_github@gadz.org>
2019-10-17 22:16:06 +0200

    V1

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 
+require 'bundler'
+Bundler.require
+
 require 'pry'   
-require_relative './lib/app'
-class Event
-  def perform
-  end
-end
-binding.pry
-  
+require 'colorize'
+require 'colorized_string'
+
+require_relative './lib/game'
+require_relative './lib/player'
+require_relative './lib/board'
+require_relative './lib/boardcase'
+require_relative './lib/show'
+require_relative './lib/application'
+
+Application.new()
